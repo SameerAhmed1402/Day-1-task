f39dd09299b4424158f78beaaced830037a17158
SameerAhmed1402 <142417658+SameerAhmed1402@users.noreply.github.com>
2023-08-16 19:35:23 +0530

    Add files via upload

---
+++ var and datatypes.js
@@ -0,0 +1,13 @@
+let a= 'sameer';
+console.log(typeof(a));
+a= 10;
+console.log(typeof(a));
+a=23.57;
+console.log(typeof(a)); // to find the datatype of variable
+a='Fruit Is an apple';
+console.log(a.length);  // string to find lenght
+console.log(a.toUpperCase()); //string to uppercase
+console.log(a.toLowerCase()); // string to lowercase
+
+
+
